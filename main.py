ec03f18e97d0bdc351e37e61a453140504fc0a40
Ivan Fedeikin <ivan.fedeikin@axxonsoft.dev>
2025-02-24 12:02:41 +0300

    add: точки сканирования для первого разведчика

--- main.py
+++ main.py
@@ -6,27 +6,47 @@ import sys
 
 if __name__ == "__main__":
     # Параметры для дрона-сканера
-    scout_drone = Pion(
-        ip="10.1.100.217",
-        mavlink_port=5656,
+    scout_drone_1 = Pion(
+        ip="127.0.0.1",
+        mavlink_port=8001,
         logger=False,
         dt=0.0,
         accuracy=0.08,
         count_of_checking_points=5
     )
     # Координаты базы
-    base_coords_scanner = np.array([3.4, 2.4, 1.0, 0])
+    base_coords_scanner = np.array([-3.92 - 2.14, 0, 0])
     # По каким координатам мы пройдемся для сканирования
-    scan_points = np.array([
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ])
+    # пролетим вдоль жд
+    h = 2
+    offset = 1.4
+    # зона сканирования - 2х2 м
+    x = -5
+    y = -5 + offset
+    scan_points_list_1 = []
+    # проход туда
+    while x < 6 or y < 6:
+        scan_points_list_1.append([x, y, h, 135])
+        x += offset * 2
+        y += offset * 2
+        pass
+    # проход обратно
+    x,y,zz,aa = scan_points_list_1[ len(scan_points_list_1)-1 ] # смещаемся от последней точки
+    x += offset
+    y -= offset
+    while x > -6 or y > -6:
+        scan_points_list_1.append([x, y, h, 315])
+        x -= offset * 2
+        y -= offset * 2
+        pass
+
+    scan_points = np.array(scan_points_list_1)
+
     # Выбираем режим камеры: для симулятора можно использовать SocketCamera,
     # для реального дрона – RTSPCamera. Здесь пример с RTSP.
-    scanner_camera = RTSPCamera(rtsp_url=f'rtsp://{scout_drone.ip}:8554/front')
-    scanner = DroneScanner(drone=scout_drone, base_coords=base_coords_scanner,
-                           scan_points=scan_points, camera=scanner_camera, show=True)
+    scanner_camera = RTSPCamera(rtsp_url=f'rtsp://{scout_drone_1.ip}:8554/front')
+    scanner_1 = DroneScanner(drone=scout_drone_1, base_coords=base_coords_scanner,
+                             scan_points=scan_points, camera=scanner_camera, show=True)
 
     # Параметры для дрона-доставщика
     delivery_drone = Pion(
@@ -38,7 +58,6 @@ if __name__ == "__main__":
         count_of_checking_points=5
     )
     base_coords_deliverer = np.array([3.5, 3.3, 1.5, 0])
-
 
     delivery_points: List[Tuple[float, float, float, float]] = [
         (0, 0, 0, 0),
